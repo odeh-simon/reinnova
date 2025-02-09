7db3d768481cd0d11cd7657d7406416ec4554253
simon odeh <simonodeh36@gmail.com>
2025-02-09 04:17:19 +0100

    fix contact bug

--- src/Modules/Contact.jsx
+++ src/Modules/Contact.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from 'react';
 import Contact from "../components/Contact"
 import Footer from "../components/Footer"
 import NavBar from "../components/NavBar"
 export default function ContactUs() {
+      // Scroll to top on component mount
+      useEffect(() => {
+        window.scrollTo(0, 0);
+      }, []);
   return (
     <div className="w-full flex flex-col gap-8">
         <NavBar />
