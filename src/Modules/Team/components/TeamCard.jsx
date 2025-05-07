1ad3d5c68d0e6d2850f16fe8e105416286f5e5f2
simon odeh <simonodeh36@gmail.com>
2025-05-07 19:11:31 +0100

    minor update

--- src/Modules/Team/components/TeamCard.jsx
+++ src/Modules/Team/components/TeamCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const TeamCard = ({ imageSrc, name, role }) => {
   return (
     <div className="flex flex-col bg-transparent rounded-b-[10px] shadow-custom-darkBrown overflow-hidden"  data-aos="fade-up">
-      <div className="flex-shrink-0">
+      <div className="flex-shrink-0 mx-auto">
         <img className="w-auto h-auto" src={imageSrc} alt={name} />
       </div>
       <div className="flex flex-col bg-[#338BAC] w-full shadow-custom-darkBrown py-2 px-5 h-[80px] relative">
