f4e779ea8a52c06e4682e9e1e9983cba4d034900
simon odeh <simonodeh36@gmail.com>
2025-05-14 11:40:38 +0100

    minor update to the gallery slider

--- src/Modules/Home/components/GallerySlider.jsx
+++ src/Modules/Home/components/GallerySlider.jsx
@@ -1,30 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSwipeable } from "react-swipeable";
 import backarrow from "../../../assets/icons/backArrow.svg";
 import forwardArrow from "../../../assets/icons/forwardArrow2.svg";
-const galleryImage1 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295931/image1_gxca87.png'
-const galleryImage2 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295935/image2_snough.png'
-const galleryImage3 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295939/image3_gdu0hr.png'
-const galleryImage4 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295944/image4_pu2eij.png'
-const galleryImage5 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451899/image5_by9udi.png'
-const galleryImage6 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451900/image6_jt0x8m.png'
-const galleryImage7 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451901/image7_mycvlz.png'
-const galleryImage8 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image8_jembpx.png'
-const galleryImage9 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image9_mvssqo.png'
-const galleryImage10 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image10_etehf3.png'
-const galleryImage11 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image11_vjwbzj.png'
-const galleryImage12 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451901/image12_ysn2vl.png'
-const galleryImage13 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451903/image13_dhjkjn.png'
-const galleryImage14 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451903/image14_oivh0d.png'
-const galleryImage15 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451903/image15_afgs89.png'
-const galleryImage16 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032160/clear-blue_helkal.png'
-const galleryImage17 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032162/green-flakes_dmpdx5.png'
-const galleryImage18 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032164/green-pet_j4fmbu.png'
-const galleryImage19 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032165/pellets_nplj8b.png'
-const galleryImage20 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032159/blue-pet_ptgeku.png'
-const galleryImage21 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032159/black-pellet2_kvfpqi.png'
-const galleryImage22 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032157/black-pellet_ra49dk.png'
-const galleryImage23 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032157/amber-flakes_qmnxpo.png'
+
+const galleryImage1 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295931/image1_gxca87.png';
+const galleryImage2 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295935/image2_snough.png';
+const galleryImage3 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295939/image3_gdu0hr.png';
+const galleryImage4 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1739295944/image4_pu2eij.png';
+const galleryImage5 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451899/image5_by9udi.png';
+const galleryImage6 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451900/image6_jt0x8m.png';
+const galleryImage7 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451901/image7_mycvlz.png';
+const galleryImage8 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image8_jembpx.png';
+const galleryImage9 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image9_mvssqo.png';
+const galleryImage10 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image10_etehf3.png';
+const galleryImage11 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451902/image11_vjwbzj.png';
+const galleryImage12 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451901/image12_ysn2vl.png';
+const galleryImage13 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451903/image13_dhjkjn.png';
+const galleryImage14 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451903/image14_oivh0d.png';
+const galleryImage15 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1743451903/image15_afgs89.png';
+const galleryImage16 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032160/clear-blue_helkal.png';
+const galleryImage17 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032162/green-flakes_dmpdx5.png';
+const galleryImage18 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032164/green-pet_j4fmbu.png';
+const galleryImage19 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032165/pellets_nplj8b.png';
+const galleryImage20 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032159/blue-pet_ptgeku.png';
+const galleryImage21 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032159/black-pellet2_kvfpqi.png';
+const galleryImage22 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032157/black-pellet_ra49dk.png';
+const galleryImage23 = 'https://res.cloudinary.com/dsnz8adqi/image/upload/v1746032157/amber-flakes_qmnxpo.png';
 
 const images = [
   galleryImage1,
@@ -52,8 +53,22 @@ const images = [
   galleryImage23,
 ];
 
+// Number of images visible at a time
+const VISIBLE_IMAGES = 4;
+
 const GallerySlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(images.length); // Start in the middle of extended array
+  const [isTransitioning, setIsTransitioning] = useState(true);
+  const containerRef = useRef(null);
+
+  // Create extended array with multiple sets of images for smoother looping
+  const extendedImages = [
+    ...images.slice(-VISIBLE_IMAGES), // Last 4 images
+    ...images,
+    ...images,
+    ...images,
+    ...images.slice(0, VISIBLE_IMAGES), // First 4 images
+  ];
 
   useEffect(() => {
     const interval = setInterval(handleNext, 2000);
@@ -61,21 +76,33 @@ const GallerySlider = () => {
   }, []);
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentIndex((prevIndex) => {
+      const newIndex = prevIndex + 1;
+      // Reset to the start of the second set of original images when reaching the end
+      if (newIndex >= extendedImages.length - VISIBLE_IMAGES * 2) {
+        setTimeout(() => {
+          setIsTransitioning(false);
+          setCurrentIndex(images.length); // Reset to start of second set
+          setTimeout(() => setIsTransitioning(true), 0);
+        }, 500); // Match transition duration
+      }
+      return newIndex;
+    });
   };
 
   const handlePrev = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + images.length) % images.length
-    );
-  };
-
-  const getVisibleImages = () => {
-    let visibleImages = [];
-    for (let i = 0; i < 4; i++) {
-      visibleImages.push(images[(currentIndex + i) % images.length]);
-    }
-    return visibleImages;
+    setCurrentIndex((prevIndex) => {
+      const newIndex = prevIndex - 1;
+      // Reset to the end of the first set of original images when reaching the start
+      if (newIndex < VISIBLE_IMAGES) {
+        setTimeout(() => {
+          setIsTransitioning(false);
+          setCurrentIndex(images.length * 2 - VISIBLE_IMAGES); // Reset to end of first set
+          setTimeout(() => setIsTransitioning(true), 0);
+        }, 500); // Match transition duration
+      }
+      return newIndex;
+    });
   };
 
   const handlers = useSwipeable({
@@ -96,41 +123,39 @@ const GallerySlider = () => {
           production process
         </p>
       </div>
-      <div className="relative flex justify-center items-center mx-auto">
+      <div className="relative flex justify-center items-center mx-auto w-full max-w-[1200px]">
         <button
           onClick={handlePrev}
-          className="absolute left-0 text-white p-2 transform -translate-x-10 hidden md:inline-block"
+          className="absolute left-0 text-white p-2 transform -translate-x-10 hidden md:inline-block z-10"
         >
           <img src={backarrow} alt="previous" />
         </button>
         <div {...handlers} className="flex overflow-hidden w-full">
-          <div className="flex transition-transform duration-300">
-            {getVisibleImages().map((image, index) => (
+          <div
+            ref={containerRef}
+            className="flex"
+            style={{
+              transform: `translateX(-${currentIndex * (100 / VISIBLE_IMAGES)}%)`,
+              width: `${(extendedImages.length / VISIBLE_IMAGES) * 100}%`,
+              transition: isTransitioning ? 'transform 500ms ease-in-out' : 'none',
+            }}
+          >
+            {extendedImages.map((image, index) => (
               <img
                 key={index}
                 src={image}
                 alt={`Slide ${index}`}
-                className="w-full sm:w-1/4 flex-shrink-0 p-3 object-cover  mb-4"
+                className="w-[25%] sm:w-[25%] flex-shrink-0 p-2 mb-4"
               />
             ))}
           </div>
         </div>
         <button
           onClick={handleNext}
-          className="absolute right-0 text-white p-2 transform translate-x-10 hidden md:inline-block"
+          className="absolute right-0 text-white p-2 transform translate-x-10 hidden md:inline-block z-10"
         >
           <img src={forwardArrow} alt="next" />
         </button>
-        {/* <div className="absolute bottom-0 mb-0 left-1/2 transform -translate-x-1/2 flex space-x-2">
-          {images.map((_, index) => (
-            <div
-              key={index}
-              className={`w-3 h-3 rounded-full ${
-                index === currentIndex ? "bg-blue-500" : "bg-transparent"
-              } border border-blue-500`}
-            />
-          ))}
-        </div> */}
       </div>
     </div>
   );
